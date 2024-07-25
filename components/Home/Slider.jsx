b3f43779b7193c51a144cd2cd5ffddf649dfcc76
dipennapit123 <dipennapit45@gmail.com>
2024-07-25 23:13:37 +0545

    Implementation of Slider

--- components/Home/Slider.jsx
+++ components/Home/Slider.jsx
@@ -1,23 +1,49 @@
-import { View, Text } from "react-native";
-import React, { useEffect } from "react";
+import { View, Text, FlatList, Image } from "react-native";
+import React, { useEffect, useState } from "react";
 import { db } from "../../config/FirebaseConfig";
 import { collection, getDocs, query } from "firebase/firestore";
 
 export default function Slider() {
+    const [sliderList, setSliderList] = useState([])
     useEffect(()=>{
         GetSliderList();
     },[])
   const GetSliderList = async () => {
+    setSliderList([]);
     const q = query(collection(db, "Slider"));
     const querySnapshot = await getDocs(q);
 
     querySnapshot.forEach((doc)=>{
         console.log(doc.data());
+        setSliderList(prev=>[...prev,doc.data()]);
     })
   };
   return (
     <View>
-      <Text>Slider</Text>
+      <Text style={{
+        fontFamily:'outfit-bold',
+        fontSize:20,
+        padding:20
+      }}>
+
+        #Special for you
+      </Text>
+      <FlatList
+      data={sliderList}
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
+      style={{paddingLeft:20,}}
+      renderItem={({item,index})=>(
+        <Image source={{uri:item.imageUrl}}
+        style={{
+            width:300,
+            height:150,
+            borderRadius:15,
+            marginRight:15
+        }}
+        />
+      )} 
+      />
     </View>
   );
 }
